d6714bf6bdcc8314270ed93adfe4e79dbfcc0408
Nichole Battleday <battleday@gmail.com>
2019-05-21 02:57:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,32 +15,55 @@ WIN_COMBINATIONS = [
   [2,5,8]
   ]
   
-  for each win_combination in WIN_COMBINATIONS
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    win_index_3 = win_combination[3]
-    win_index_3 = win_combination[4]
-    win_index_3 = win_combination[5]
-    win_index_3 = win_combination[6]
-    win_index_3 = win_combination[7]
-    
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    position_4 = board[win_index_4]
-    position_5 = board[win_index_5]
-    position_6 = board[win_index_6]
-    position_7 = board[win_index_7]
-    
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
+  def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
   else
-    false
+    return false
   end
-  if position_4 == "X" && position_5 == "X" && position_6 == "X"
-    return win_combination
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
   else
-    false
+    return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
 end
