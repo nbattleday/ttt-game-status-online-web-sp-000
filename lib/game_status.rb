bc1d9b70df3d6a4b45a3a02eeb15666e2985522f
Nichole Battleday <battleday@gmail.com>
2019-05-21 02:56:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,43 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+  ]
+  
+  for each win_combination in WIN_COMBINATIONS
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    win_index_3 = win_combination[3]
+    win_index_3 = win_combination[4]
+    win_index_3 = win_combination[5]
+    win_index_3 = win_combination[6]
+    win_index_3 = win_combination[7]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    position_4 = board[win_index_4]
+    position_5 = board[win_index_5]
+    position_6 = board[win_index_6]
+    position_7 = board[win_index_7]
+    
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination
+  else
+    false
+  end
+  if position_4 == "X" && position_5 == "X" && position_6 == "X"
+    return win_combination
+  else
+    false
+  end
+end
